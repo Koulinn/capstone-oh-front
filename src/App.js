1717f2620217843f4084892d4962f9fc2b6d68ed
Koulinn <drdverzola@gmail.com>
2021-10-25 18:22:32 +0100

    added routes /, /register, login, booking

--- src/App.js
+++ src/App.js
@@ -3,24 +3,41 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Home from './views/Home/Home';
 import NavigationBar from './components/Navigation/NavigationBar';
+import Register from './views/Register/Register';
+import Login from './views/Login/Login';
+import Booking from './views/Booking/Booking';
+import { withRouter } from "react-router";
+import { useState } from 'react'
+
 
 
 // const ADDRESS = process.env.REACT_APP_API_URL
 // export const socket = io(ADDRESS, { transports: ['websocket'] })
 
-
 function App() {
 
   return (
-    <Container className="position-relative">
-    <Router>
-      <NavigationBar/>
-      <Route path="/" exact render={(routerProps) =>
-              <Home {...routerProps} />}>
-        </Route>
-    </Router>
-    
-    </Container>
+    <Container className="overflow-hidden" fluid>
+
+      <Container className="position-relative p-outside">
+        <Router>
+          <NavigationBar />
+          <Route path="/" exact render={(routerProps) =>
+            <Home {...routerProps} />}>
+          </Route>
+          <Route path="/register" exact render={(routerProps) =>
+            <Register {...routerProps} />}>
+          </Route>
+          <Route path="/login" exact render={(routerProps) =>
+            <Login {...routerProps} />}>
+          </Route>
+          <Route path="/booking" exact render={(routerProps) =>
+            <Booking {...routerProps} />}>
+          </Route>
+        </Router>
+
+      </Container>
+    </Container >
   );
 }
 
