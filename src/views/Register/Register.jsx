90c7f04b589679f3b70f22b30ce9e300a160ecd3
Koulinn <drdverzola@gmail.com>
2021-10-25 18:28:22 +0100

    Register component

---
+++ src/views/Register/Register.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Row} from 'react-bootstrap'
+import Welcome from '../../components/Register/Welcome/Welcome'
+import { ReactComponent as RegisterIMG } from '../../assets/Doctor prescribing drug to happy family.svg';
+import {useState} from 'react'
+import RegisterSteps from '../../components/Register/RegisterSteps/RegisterSteps';
+
+
+
+
+
+
+function Register() {
+    const [viewController, setViewController] = useState({
+        welcome:true,
+        steps:false,
+        success:false,
+    })
+
+    const [userData, setUserData] = useState({
+        name: '',
+        surname: '',
+        email: '',
+        password: '',
+        phone_primary: ''
+    })
+    
+    
+    
+    return (
+        
+            <Row className="box-shadow my-5 overflow-hidden">
+                {viewController.welcome? <Welcome 
+                    title="Welcome to OneHealth!"
+                    subLabel="Register with e-mail"
+                    option="Already have an account? Go to "
+                    redirect="Login"
+                    url="/login"
+                    img={<RegisterIMG/>}
+                    setViewController={setViewController}
+                    viewController={viewController}
+                /> : ''}
+                {viewController.steps ? <RegisterSteps userData={userData} setUserData={setUserData}/> : ''}
+            </Row>
+            
+       
+    )
+}
+
+export default Register
