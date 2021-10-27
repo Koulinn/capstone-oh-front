72591f14d7a1d2b41becf3f9d5a8b76034bcc4b7
Koulinn <drdverzola@gmail.com>
2021-10-27 14:00:40 +0100

    refactored facility

--- src/components/BookingProcess/Facility.jsx
+++ src/components/BookingProcess/Facility.jsx
@@ -1,38 +1,66 @@
 import React from 'react'
 import GP_LIST from '../../views/Booking/GPLists'
+import ConfirmStepsBtn from './ConfirmStepsBtn'
 
 
-function Facility({setFacility, facility}) {
+function Facility({ setFacility, facility, setBookingSteps }) {
 
     const facilityHandler = (facility) => {
         setFacility(facility)
     }
 
-   
+    const confirmFacility = () => {
+        setBookingSteps({
+            medicalTests: false,
+            facility: false,
+            generalAvailability: true,
+            pickDate: false,
+            checkPersonalDetails: false,
+            successScreen: false
+        })
+    }
+    const returnStep = () => {
+        setBookingSteps({
+            medicalTests: true,
+            facility: false,
+            generalAvailability: false,
+            pickDate: false,
+            checkPersonalDetails: false,
+            successScreen: false
+        })
+    }
+
     return (
         <>
             <h2 className="text-center mt-5 mb-3">Choose a location</h2>
-                    <ul className="facility-wrapper w-75">
-                        {GP_LIST.map((gp, index) =>
-                            <li className={'my-3 cursor-pointer'} key={index} onClick={() => facilityHandler(gp)}>
-                                <div className={"d-flex flex-column p-3 facility " + (facility?.name === gp?.name ? ' bg-selected' : '')}>
-                                    <div className="d-flex justify-content-between">
-                                        <h5>{gp.name}</h5>
-                                        <span>
-                                            {gp.dist}
-                                            <small>
-                                                km
-                                            </small>
-                                        </span>
-                                    </div>
-                                    <p className="my-2">{gp.address}</p>
-                                    <p>{gp.postCode}</p>
-                                    <h6 className="mt-2">{gp.phone}</h6>
-                                </div>
-                                <hr className="w-50" />
-                            </li>
-                        )}
-                    </ul>
+            <ul className="facility-wrapper w-75">
+                {GP_LIST.map((gp, index) =>
+                    <li className={'my-3 cursor-pointer'} key={index} onClick={() => facilityHandler(gp)}>
+                        <div className={"d-flex flex-column p-3 facility " + (facility?.name === gp?.name ? ' bg-selected' : '')}>
+                            <div className="d-flex justify-content-between">
+                                <h5>{gp.name}</h5>
+                                <span>
+                                    {gp.dist}
+                                    <small>
+                                        km
+                                    </small>
+                                </span>
+                            </div>
+                            <p className="my-2">{gp.address}</p>
+                            <p>{gp.postCode}</p>
+                            <h6 className="mt-2">{gp.phone}</h6>
+                        </div>
+                        <hr className="w-50" />
+                    </li>
+                )}
+            </ul>
+            
+                <ConfirmStepsBtn
+                    stepsController={confirmFacility}
+                    btnText='Confirm place'
+                    stepsReturn={returnStep}
+                />
+            
         </>
     )
 }
