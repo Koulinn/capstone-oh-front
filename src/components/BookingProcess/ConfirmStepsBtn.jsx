0331b31fdecfdddd2c0d15776884331ef15bb9cf
Koulinn <drdverzola@gmail.com>
2021-10-27 14:00:31 +0100

    added curso point to return btn

--- src/components/BookingProcess/ConfirmStepsBtn.jsx
+++ src/components/BookingProcess/ConfirmStepsBtn.jsx
@@ -5,7 +5,7 @@ function ConfirmStepsBtn({ stepsController, btnText, stepsReturn }) {
     return (
         <div className={"mt-5 d-flex w-75" + (stepsReturn ? ' justify-content-between' : ' justify-content-center')}>
             {stepsReturn ?
-                <div className="d-flex align-items-center" onClick={stepsReturn}>
+                <div className="d-flex align-items-center cursor-pointer" onClick={stepsReturn}>
                     <span>Return</span>
                 </div>
                 : ''}
