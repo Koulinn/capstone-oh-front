d640d11e715a3e74acf052443e27f213c19d8274
Koulinn <drdverzola@gmail.com>
2021-10-27 10:49:24 +0100

    removed register and added setuserLogin

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,6 +4,10 @@ export const setUserData = (payload) => ({
   type: 'SET_USER_DATA',
   payload: payload
 })
+export const setUserLogIn = () => ({
+  type: 'SET_LOGGED_IN',
+})
+
 export const setUserTokens = (payload) => ({
   type: 'SET_USER_TOKENS',
   payload: payload
@@ -35,25 +39,3 @@ export const setLogin = (email, password) => {
   }
 }
 
-export const registerUser = ({name, email, password}) => {
-  return async (dispatch) => {
-    const baseUrl = `${process.env.REACT_APP_PROD_API_URL}user/register`
-    try {
-      let response = await axios.post(baseUrl, { name: name, email: email, password: password }, { withCredentials: true })
-      if (response.status === 201) {
-    
-        dispatch({
-          type: 'SET_LOGGED_IN',
-        })
-      } else {
-        dispatch({
-          type: 'SET_LOGGED_OUT',
-        })
-      }
-    } catch (error) {
-      dispatch({
-        type: 'SET_LOGGED_OUT',
-      })
-    }
-  }
-}
