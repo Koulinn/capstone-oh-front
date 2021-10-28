cf9a71b6976e6248eebf8b82f4e22b333ab72326
Koulinn <drdverzola@gmail.com>
2021-10-28 16:49:18 +0100

    added login login to dashboard component

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -4,28 +4,27 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Button, Row } from 'react-bootstrap'
 import { Avatar, Badge } from '@mui/material';
 import request from '../../lib/requests-authenticated.js'
-import { setUserData } from '../../redux/actions/index.js';
+import { setUserData, setUserTokens } from '../../redux/actions/index.js';
 import ProfileAvatar from '../../components/Profile/ProfileAvatar.jsx';
 import ProfileMeta from '../../components/Profile/ProfileMeta.jsx';
 import ProfileActions from '../../components/Profile/ProfileActions.jsx';
+import RequestCard from '../../components/Profile/RequestCard.jsx';
+
 
 const { getMe } = request
 
-const BGURL='https://res.cloudinary.com/koulin/image/upload/v1635416824/OneHealth/profileBG_xavs0i.svg'
+const BGURL = 'https://res.cloudinary.com/koulin/image/upload/v1635416824/OneHealth/profileBG_xavs0i.svg'
 
-function Dashboard({ history }) {
+function Dashboard({ history, location }) {
     const isLogged = useSelector(s => s.user.isLogged)
     const user = useSelector(s => s.user)
     const dispatch = useDispatch()
 
     const { avatar, name, surname, email, phone_primary, medical_tests_requested } = user
 
-    const asyncWrapper = async () => {
+    const asyncWrapper = async (token= undefined) => {
         try {
-            const res = await getMe()
-            if (res.status === 200) {
-                dispatch(setUserData(res.data.user))
-            }
+            const res = await getMe(token)
 
         } catch (error) {
             console.log(error)
@@ -34,8 +33,17 @@ function Dashboard({ history }) {
     }
 
     useEffect(() => {
-        if (isLogged) {
-            asyncWrapper()
+        const isAccessToken = new URLSearchParams(location.search).get("accessToken")
+        const isRefreshToken = new URLSearchParams(location.search).get("refreshToken")
+        if (isLogged || isAccessToken) {
+            asyncWrapper(isAccessToken)
+
+            if(isAccessToken){
+                dispatch(setUserTokens({
+                    accessToken: isAccessToken,
+                    refreshToken: isRefreshToken
+                }))
+            }
         } else {
             history.push('/')
         }
@@ -43,13 +51,13 @@ function Dashboard({ history }) {
     return (
         <Row className="box-shadow my-5 overflow-hidden mx-1">
             <div className="profile-wrapper col-12 col-md-6 d-flex flex-column flex-center-center">
-                <div className="avatar-wrapper d-flex flex-center-center position-relative" style={{backgroundImage: `url(${BGURL})`}}>
+                <div className="avatar-wrapper d-flex flex-center-center position-relative" style={{ backgroundImage: `url(${BGURL})` }}>
                     <ProfileAvatar
                         medical_tests_requested={medical_tests_requested}
                         name={name}
-                        surname={surname} 
+                        surname={surname}
                         avatar={avatar}
-                        />
+                    />
                 </div>
                 <div className="profile-meta-wrapper mt-4">
                     <ProfileMeta
@@ -59,15 +67,26 @@ function Dashboard({ history }) {
                         phone_primary={phone_primary}
                     />
                 </div>
-                <div className="profile-actions flex-center-center flex-column mt-5 w-100">
-                    <ProfileActions/>
+                <div className="profile-actions flex-center-center flex-column my-5 w-100">
+                    <ProfileActions />
 
                 </div>
 
 
             </div>
-            <div className="col-12 col-md-6 my-sm-5">
-                <h4 className="text-center">Recent results</h4>
+            <div className="col-12 col-md-6 my-sm-5 requests-wrapper">
+                <div className="pending-requests-wrapper">
+                    <h4 className="text-center">Pending requests</h4>
+                    <div className="pending-requests-cards-wrapper">
+                        {medical_tests_requested.map(req => {
+                            const tags = req.user_tests_requested.userTestsTags
+                            const imgs = req.user_tests_requested.userFilesURL
+                            return <RequestCard key={req._id} imgsPreview={imgs} requestTags={tags}  />
+
+                        })}
+                    </div>
+                </div>
+
 
             </div>
         </Row>
