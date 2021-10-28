af90d3e00baf0b651daeb9df265c0741e2f92edf
Koulinn <drdverzola@gmail.com>
2021-10-28 16:49:50 +0100

    added booking and logout logic

--- src/components/Profile/ProfileActions.jsx
+++ src/components/Profile/ProfileActions.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
-function ProfileActions() {
+import {withRouter} from 'react-router-dom'
+import { BASE_URL } from '../../lib/index.js'
+import store from '../../redux/store/index.js'
+
+function ProfileActions({history}) {
+
+    const logout =() =>{
+        localStorage.clear()
+        window.location.replace('http://localhost:3000/')
+
+    }
+
+    const booking =()=>{
+        history.push('/booking')
+    }
     return (
         <>
             <Button className="mt-5">Check results</Button>
-            <Button variant="outline" className="border my-4">Book new test</Button>
-            <Button variant="outline" className="mt-5">Logout</Button>
+            <Button variant="outline" className="border my-4" onClick={booking}>Book new test</Button>
+            <Button variant="outline" className="mt-5" onClick={logout}>Logout</Button>
         </>
     )
 }
 
-export default ProfileActions
+export default withRouter(ProfileActions)
