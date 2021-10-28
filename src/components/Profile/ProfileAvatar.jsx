a255e64698cde56e15a8678373bc1727d5bfa945
Koulinn <drdverzola@gmail.com>
2021-10-28 16:50:01 +0100

    created avatar component

---
+++ src/components/Profile/ProfileAvatar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Avatar, Badge } from '@mui/material';
+
+export default function ProfileAvatar({ medical_tests_requested, name, avatar }) {
+    return (
+        <Badge
+            color='primary'
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            variant="dot"
+            invisible={medical_tests_requested?.length > 0 ? false : true}
+        >
+            <label htmlFor="avatar" className="cursor-pointer"><Avatar alt={name + ' picture'} src={avatar} /></label>
+            <input className="d-none position-absolute" id='avatar' type="file" />
+        </Badge>
+    )
+}
