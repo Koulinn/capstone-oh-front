4535b70d00de3ca4e7a497eeea07a10e5a467e02
Koulinn <drdverzola@gmail.com>
2021-10-29 15:44:45 +0100

    cleaned code

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -12,30 +12,8 @@ export const setUserTokens = (payload) => ({
   type: 'SET_USER_TOKENS',
   payload: payload
 })
+export const setUserLogOut = (payload) => ({
+  type: 'SET_LOGGED_OUT',
+})
 
-export const setLogin = (email, password) => {
-
-  return async (dispatch, getState) => {
-    const baseUrl = `${process.env.REACT_APP_PROD_API_URL}user/login`
-    
-    try {
-      let response = await axios.post(baseUrl, { email: email, password: password }, { withCredentials: true })
-      if (response.status === 200) {
-        
-        dispatch({
-          type: 'SET_LOGGED_IN',
-        })
-      } else {
-        dispatch({
-          type: 'SET_LOGGED_OUT',
-        })
-
-      }
-    } catch (error) {
-      dispatch({
-        type: 'SET_LOGGED_OUT',
-      })
-    }
-  }
-}
 
