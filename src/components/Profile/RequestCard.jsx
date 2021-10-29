fb6af57d5594e15e3a41d5d8a84e1cbac408e6af
Koulinn <drdverzola@gmail.com>
2021-10-29 15:43:22 +0100

    refactored Request Card

--- src/components/Profile/RequestCard.jsx
+++ src/components/Profile/RequestCard.jsx
@@ -1,59 +1,39 @@
 import React from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import { Badge } from '@mui/material';
 
-function RequestCard({imgsPreview, requestTags}) {
+function RequestCard({ imgsPreview, requestTags, location }) {
     return (
-        <Card>
-            <CardContent>
-                <div className="d-flex flex-column">
-                   <div className="d-flex justify-content-between align-items-center">
-                        <h6>
-                            Date received: <strong>15/05/2021</strong>
-                        </h6>
-                        <div className="d-flex align-items-center">
-                            <span className="mr-3">Processing</span>
-                            <Badge
-                                
-                                color='warning'
-                                variant='dot'
-                                overlap="circular"
-                            />
-                        </div>
-                   </div>
-                    <div className="my-2">
-                        <p>Location: <strong>Mario</strong></p>
-                    </div>
-                    <div>
-                        <p>Medical tests</p>
-                        <div className={"my-3 tag-preview-wrapper"}>
-                            {requestTags.length > 0 ?
-                                <ul className="d-flex justify-content-start flex-wrap row">
-                                    {requestTags.map((tag, index) =>
+        <>
+            <div className="d-flex flex-column">
+                <div className="mb-2">
+                    <p>Location: <strong>{location}</strong></p>
+                </div>
+                <div>
+                    <p>Medical tests</p>
+                    <div className={"my-3 tag-preview-wrapper"}>
+                        {requestTags.length > 0 ?
+                            <ul className="d-flex justify-content-start flex-wrap row">
+                                {
+                                    requestTags.map((tag, index) =>
                                         <li
-                                            key={index}
+                                            key={index + 200}
                                             className="p-2"
                                         >
                                             {tag}
-                                        </li>)}
-                                </ul>
-                                : ''}
-                        </div>
-                        <div className={"imgsPreview-wrapper justify-content-between row "}>
-                            {imgsPreview.map((i, index) => <>
-                                <div className="position-relative col-md-6 col-lg-4 my-3 p-0">
-                                    <img className="" height="96" key={index} src={i} /> 
-                                </div>
-                            </>)}
-                        </div>
+                                        </li>)
+                                }
+                            </ul>
+                            : ''}
+                    </div>
+                    <div className={"imgsPreview-wrapper justify-content-between row "}>
+                        {imgsPreview.map((i, index) => 
+                            <div className="position-relative col-md-6 col-lg-4 my-3 p-0" key={index + 300}>
+                                <img className="" height="96" src={i} />
+                            </div>
+                        )}
                     </div>
                 </div>
-
-            </CardContent>
-
-        </Card>
+            </div>
+        </>
     )
 }
 
