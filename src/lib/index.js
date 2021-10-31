32284695f04bb092f8271227ae23695d35aca87b
Koulinn <drdverzola@gmail.com>
2021-10-31 18:13:04 +0000

    removed comments

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,10 @@
 export const BASE_URL = process.env.REACT_APP_API_URL
 
 
-
-// setUserImagePath(URL.createObjectURL(e.target.files[0]))
-//                                 newUserImage.append('image', e.target.files[0])
-
-// images should be array of files
-// form should be formData obj
 export const appendImgs =(form, images)=>{
     images.forEach(i=> form.append('medicalRequestsImgs', i ) )
     return form
 } 
-// images should be array of files
 export const previewUrls =(images)=>{
     const previewList = images.map(i=> URL.createObjectURL(i) )
     return previewList
