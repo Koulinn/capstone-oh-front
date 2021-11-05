1365aae9463f457c5d1aceb37d2e3ae1fa8b2b39
Koulinn <drdverzola@gmail.com>
2021-11-05 09:31:17 +0000

    created booking steps

---
+++ src/views/Booking/bookingSteps.js
@@ -0,0 +1,22 @@
+const steps = [
+    {
+      label: 'Medical tests',
+      description: `You can upload the medical tests request or add the tests manually`,
+    },
+    {
+      label: 'Medical facility',
+      description:
+        'We always have a medical facility next to you',
+    },
+    {
+      label: 'Availability',
+      description:
+        'You can pick up to 5 dates that bests suits you',
+    },
+    {
+      label: 'Contact confirmation',
+      description:
+        'After we receive your request we will get in touch to confirm and orient your for the medical tests',
+    }
+  ];
+export default steps
