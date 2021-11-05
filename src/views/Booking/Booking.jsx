bafc927dfa8acd7aa2ac5371de98169e26e6b67e
Koulinn <drdverzola@gmail.com>
2021-11-05 09:30:58 +0000

    Refactored medical tests preview

--- src/views/Booking/Booking.jsx
+++ src/views/Booking/Booking.jsx
@@ -12,6 +12,7 @@ import Availability from '../../components/BookingProcess/Availability'
 import ConfirmDetails from '../../components/BookingProcess/ConfirmDetails'
 import FacilityCard from '../../components/BookingProcess/FacilityCard'
 import Success from '../../components/Success/Success'
+import TestsPreview from '../../components/BookingProcess/BookingPreview/TestsPreview'
 
 const { getMe } = requests
 const successImg = "https://res.cloudinary.com/koulin/image/upload/v1635614779/OneHealth/successOH_wxysls.svg"
@@ -145,37 +146,9 @@ function Booking({ history }) {
 
             {!bookingSteps.successScreen ? <>
                 <div className="request-info-wrapper d-none d-md-block col-6 mt-5">
-                    <h3 className="text-center">Request</h3>
-                    <div className={'w-75 mx-auto'}>
-                        <h5 className="text-center pt-2">Medical tests</h5>
-                        <div className={"imgsPreview-wrapper justify-content-between row "}>
-                            {imgsPreview.map((i, index) => <>
-                                <div  key={index} className="position-relative col-md-6 col-lg-4 my-3 p-0">
-                                    <img className="" height="96" src={i} />
-                                    <div className="position-absolute d-flex flex-center-center" onClick={() => removeImg(index)}>
-                                        <MdRemove />
-                                    </div>
-                                </div>
-
-                            </>)}
-                        </div>
-                        <div className={"my-3 tag-preview-wrapper"}>
-                            {requestTags.length > 0 ?
-                                <ul className="d-flex justify-content-start flex-wrap row">
-                                    {requestTags.map((tag, index) =>
-                                        <li
-                                            key={index}
-                                            className="p-2"
-                                        >
-                                            {tag}
-                                            <span className="ml-3 mr-1" onClick={() => removeTag(tag, requestTags, setRequestTags)}>
-                                                <MdRemove />
-                                            </span>
-                                        </li>)}
-                                </ul>
-                                : ''}
-                        </div>
-                    </div>
+                    <TestsPreview
+                        imgsPreview={imgsPreview} removeImg={removeImg} requestTags={requestTags} setRequestTags={setRequestTags}
+                    />
                     {facility ?
                         <div className="chosen-location-wrapper mt-5 w-75 mx-auto">
                             <h5 className="text-center mb-1">Location</h5>
