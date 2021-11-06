cd3cd8ce166d39c57bf6fdebbf157a62c3c543c4
Koulinn <drdverzola@gmail.com>
2021-11-06 06:27:35 +0000

    removed bars from from 1140 and lower

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,11 +3,13 @@ import Hero from '../../components/Hero/Hero'
 import Bars from '../../components/Bars/Bars'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
 const heroImgURL = "https://res.cloudinary.com/koulin/image/upload/v1635008297/OneHealth/Website/illus1_lyd6k3.png"
 const title = 'Booking medical tests never was so easy'
 function Home({history}) {
+    const mediaLarge = useMediaQuery('(min-width:1140px)');
     const isLogged=useSelector(s=>s.user.isLogged)
 
     useEffect(()=>{
@@ -19,8 +21,11 @@ function Home({history}) {
     },[])
     return (
         <div>
-            <Bars time={5350} pos={''}/>
-            <Bars time={0} pos={'bars-horizontal'}/>
+            {mediaLarge ? 
+            <>
+                <Bars time={5350} pos={''}/>
+                <Bars time={0} pos={'bars-horizontal'}/>
+            </> : ''}
             <Hero title={title} heroImgURL={heroImgURL} history={history}/>
             
         </div>
