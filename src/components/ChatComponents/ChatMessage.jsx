91fd4611286c33be17238d5e5ea1d3d463d4521b
Koulinn <drdverzola@gmail.com>
2021-11-07 10:40:27 +0000

    date format and chat messaged spacing solved

--- src/components/ChatComponents/ChatMessage.jsx
+++ src/components/ChatComponents/ChatMessage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {format} from 'date-fns'
 
 function ChatMessage({ user, message, img }) {
     return (
         <div className={"position-relative w-75 my-3" + (user ? ' align-self-end' : '')}>
-            <div className={"p-3 d-block text-truncate" + (user ? ' userMessage-wrapper' : ' assistantMessage-wrapper')} >
+            <div className={"px-3 pt-3 pb-2 d-block" + (user ? ' userMessage-wrapper' : ' assistantMessage-wrapper')} >
                 {img ?
                     <img
                         className="mb-2"
@@ -12,8 +13,8 @@ function ChatMessage({ user, message, img }) {
                     />
                     : ''
                 }
-                <p className="text-truncate">{message.text}</p>
-                <span>{message.createdAt}</span>
+                <p style={{overflowWrap: 'anywhere'}}>{message.text}</p>
+                <small className="d-block" style={{textAlign:'right'}}>{format(new Date(message.createdAt), 'HH:mm')}</small>
             </div>
 
         </div>
