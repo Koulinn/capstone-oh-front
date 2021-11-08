fda2bc45296ed327f7cf114d40fdea46197b562a
Koulinn <drdverzola@gmail.com>
2021-11-08 14:42:19 +0000

    refactored phone number inputs

--- src/components/PersonalData/PersonalData.jsx
+++ src/components/PersonalData/PersonalData.jsx
@@ -3,9 +3,8 @@ import { useFormik } from "formik";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 import TextField from "@material-ui/core/TextField";
+import MuiPhoneInput from 'material-ui-phone-number';
 
-var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-const re = /^[0-9\b]+$/;
 const validate = (values) => {
   const errors = {};
 
@@ -16,51 +15,35 @@ const validate = (values) => {
   if (!values.surname) {
     errors.surname = "Required";
   }
-  if (!values.phone_primary) {
-    errors.phone_primary = "Required";
-  } else if (!re.test(values.phone_primary)) {
-    errors.phone_primary = "Only numbers";
-  } else if (
-    values.phone_primary.length < 7 ||
-    values.phone_primary.length > 10
-  ) {
-    errors.phone_primary = "Sorry wrong format must have between 7-10 digits";
-  } else if (values.phone_primary.match(phoneno)) {
-    errors.phone_primary = `It doesn't look like a phone number`;
-  }
-
   return errors;
 };
 
-function PersonalData({ 
-    userData, 
-    setUserData,
-     setSteps, 
-     setViewController,
-     handleNext,
-     handleReset 
-    }) {
+function PersonalData({
+  userData,
+  setUserData,
+  setSteps,
+  setViewController,
+  handleNext,
+  handleReset
+}) {
   const [showAlert, setShowAlert] = useState(false);
   const formik = useFormik({
     initialValues: {
       name: "",
       surname: "",
-      phone_primary: "",
     },
     validate,
-    onSubmit: (values) => {
-      // const normalizedPhone = phone(values.phone_primary, { country: 'UK' })
+    onSubmit: (values) => {   
       setUserData({
         ...userData,
         ...values,
-        // phone_primary: normalizedPhone
       });
       handleNext()
       setSteps({
         personalData: false,
         accessData: true,
       });
-      
+
     },
   });
 
@@ -71,6 +54,16 @@ function PersonalData({
       steps: false,
     });
   };
+
+  const getPhoneNumber =(value)=>{
+    const phoneFormatted = value.substring(1, value.length)
+    console.log(phoneFormatted)
+    
+    setUserData({
+      ...userData,
+      'phone_primary' : phoneFormatted
+    })
+  }
 
   return (
     <>
@@ -98,20 +91,15 @@ function PersonalData({
           error={formik.touched.surname && Boolean(formik.errors.surname)}
           helperText={formik.touched.surname && formik.errors.surname}
         />
-        <TextField
+        <MuiPhoneInput
+          defaultCountry='it'
+          regions={'europe'}
           fullWidth
           id="phone_primary"
           name="phone_primary"
           label="Phone number"
           type="phone_primary"
-          value={formik.values.phone_primary}
-          onChange={formik.handleChange}
-          error={
-            formik.touched.phone_primary && Boolean(formik.errors.phone_primary)
-          }
-          helperText={
-            formik.touched.phone_primary && formik.errors.phone_primary
-          }
+          onChange={getPhoneNumber}
         />
         <div className="my-5 d-flex justify-content-between align-items-center">
           <div className="cursor-pointer py-3" onClick={returnHome}>
--- src/views/CompleteRegistration/CompleteRegistration.jsx
+++ src/views/CompleteRegistration/CompleteRegistration.jsx
@@ -1,79 +1,58 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { useFormik } from "formik";
 import { useEffect, useState } from 'react'
 import { Button } from "react-bootstrap";
-import TextField from "@material-ui/core/TextField";
 import requests from '../../lib/requests-authenticated';
 import { setUserTokens } from '../../redux/actions/index.js';
-import {useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import Spinner from '../../components/Spinner/Spinner'
 import Alerts from '../../components/Alerts/Alerts.jsx';
+import MuiPhoneInput from 'material-ui-phone-number';
 
-var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-const re = /^[0-9\b]+$/;
-const validate = (values) => {
-    const errors = {};
-    if (!values.phone_primary) {
-        errors.phone_primary = "Required";
-    } else if (!re.test(values.phone_primary)) {
-        errors.phone_primary = "Only numbers";
-    } else if (
-        values.phone_primary.length < 7 ||
-        values.phone_primary.length > 10
-    ) {
-        errors.phone_primary = "Sorry wrong format must have between 7-10 digits";
-    } else if (values.phone_primary.match(phoneno)) {
-        errors.phone_primary = `It doesn't look like a phone number`;
-    }
-
-    return errors;
-};
-
-function CompleteRegistration({location, history}) {
+function CompleteRegistration({ location, history }) {
     const dispatch = useDispatch()
     const [showError, setShowError] = useState(false)
     const [isSpinning, setIsSpinning] = useState(false)
 
-    const formik = useFormik({
-        initialValues: {
-            phone_primary: "",
-        },
-        validate,
-        onSubmit: async (values) => {
-            setIsSpinning(true)
-            const res = await sendPhoneNumber(values)
-            if(res.status === 200){
+    const sendInput = async (e) => {
+        e.preventDefault()
+        console.log(e)
+        const phoneFormatted = {
+            'phone_primary': e.target[1].value.substring(1, e.target[1].value.length)
+        }
 
-                setShowError(false)
-                setTimeout(async () => {
-                    await requests.getMe()
-                    
-                    setIsSpinning(false)
-                    history.push('/dashboard')
-                }, 1500)
-                
-            } else{
+        setIsSpinning(true)
+        const res = await sendPhoneNumber(phoneFormatted)
+        if (res.status === 200) {
+
+            setShowError(false)
+            setTimeout(async () => {
+                await requests.getMe()
+
                 setIsSpinning(false)
-                setShowError(true)
-                setTimeout(() => setShowError(false), 3000)
-            }
-        },
-    });
+                history.push('/dashboard')
+            }, 1500)
 
-    const sendPhoneNumber = async(phoneNumber)=>{
+        } else {
+            setIsSpinning(false)
+            setShowError(true)
+            setTimeout(() => setShowError(false), 3000)
+        }
+    };
+
+    const sendPhoneNumber = async (phoneNumber) => {
         try {
             const res = await requests.updateUserData(phoneNumber)
             return res
         } catch (error) {
             setIsSpinning(false)
-                setShowError(true)
-                setTimeout(() => setShowError(false), 3000)
-            console.log(error)  
+            setShowError(true)
+            setTimeout(() => setShowError(false), 3000)
+            console.log(error)
         }
     }
 
-    const getTokens = ()=>{
+    const getTokens = () => {
         const isAccessToken = new URLSearchParams(location.search).get("accessToken")
         const isRefreshToken = new URLSearchParams(location.search).get("refreshToken")
         dispatch(setUserTokens({
@@ -82,38 +61,32 @@ function CompleteRegistration({location, history}) {
         }))
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         getTokens()
     }, [])
     return (
         <Row className={"box-shadow my-5 flex-center-center flex-column overflow-hidden"}>
-            <Col xs="12" md="12" lg="6" style={{maxWidth:"328px", padding:"0"}} >
+            <Col xs="12" md="12" lg="6" style={{ maxWidth: "328px", padding: "0" }} >
                 <h2 className={"mt-5"}>Complete registration</h2>
-                <form className="d-flex flex-column" onSubmit={formik.handleSubmit}>
-                    <TextField
+                <form className="d-flex flex-column" onSubmit={(e)=>sendInput(e)}>
+                    <MuiPhoneInput
+                    className="mt-3"
+                        defaultCountry='it'
+                        regions={'europe'}
                         fullWidth
-                        className="my-4"
                         id="phone_primary"
                         name="phone_primary"
                         label="Phone number"
                         type="phone_primary"
-                        value={formik.values.phone_primary}
-                        onChange={formik.handleChange}
-                        error={
-                            formik.touched.phone_primary && Boolean(formik.errors.phone_primary)
-                        }
-                        helperText={
-                            formik.touched.phone_primary && formik.errors.phone_primary
-                        }
                     />
                     <div className="mt-3 w-100">
-                            {isSpinning && <Spinner />}
-                            {showError && <Alerts
-                                title='We got an error'
-                                message='Sorry, something went wrong'
-                                state="danger" />
-                            }
-                        </div>
+                        {isSpinning && <Spinner />}
+                        {showError && <Alerts
+                            title='We got an error'
+                            message='Sorry, something went wrong'
+                            state="danger" />
+                        }
+                    </div>
                     <Button variant={"primary"} className="w-50 align-self-end mb-5" type="submit">
                         Register
                     </Button>
