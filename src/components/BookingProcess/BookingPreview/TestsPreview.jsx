364ea2edcd3c330e21b02af6e02e5739b9fa4b82
Koulinn <drdverzola@gmail.com>
2021-11-10 11:28:42 +0000

    Implemented Mui chips for the lists

--- src/components/BookingProcess/BookingPreview/TestsPreview.jsx
+++ src/components/BookingProcess/BookingPreview/TestsPreview.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { removeTag } from '../../../lib'
 import { MdRemove } from 'react-icons/md'
+import Chip from '@mui/material/Chip';
 
 function TestsPreview({ imgsPreview, removeImg, requestTags, setRequestTags }) {
     return (
         <div className={'w-100'}>
             <div className={"imgsPreview-wrapper row "}>
-                {imgsPreview.map((i, index) => 
-                
+                {imgsPreview.map((i, index) =>
+
                     <div key={index} className="position-relative col-md-6 col-lg-4 p-0">
                         <img className="" height="96" src={i} />
                         <div className="position-absolute d-flex flex-center-center" onClick={() => removeImg(index)}>
@@ -18,20 +19,19 @@ function TestsPreview({ imgsPreview, removeImg, requestTags, setRequestTags }) {
                 )}
             </div>
             <div className={"tag-preview-wrapper"}>
-                {requestTags.length > 0 ?
+                {requestTags.length &&
                     <ul className="d-flex justify-content-start flex-wrap row">
-                        {requestTags.map((tag, index) =>
-                            <li
-                                key={index}
-                                className="p-2"
-                            >
-                                {tag}
-                                <span className="ml-3 mr-1" onClick={() => removeTag(tag, requestTags, setRequestTags)}>
-                                    <MdRemove />
-                                </span>
-                            </li>)}
+                            {requestTags.map((tag, index) =>
+                                <Chip
+                                    key={index}
+                                    className="m-1"
+                                    label={tag}
+                                    variant="outlined"
+                                    onDelete={()=>removeTag(tag, requestTags, setRequestTags)}
+                                />
+                            )}
                     </ul>
-                    : ''}
+                    }
             </div>
         </div>
     )
--- src/components/Profile/RequestCard.jsx
+++ src/components/Profile/RequestCard.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
+import React from "react";
+import Chip from "@mui/material/Chip";
 
 function RequestCard({ imgsPreview, requestTags, location }) {
-    return (
-        <>
-            <div className="d-flex flex-column">
-                <div className="mb-2">
-                    <p>Location: <strong>{location}</strong></p>
-                </div>
-                <div>
-                    <p>Medical tests</p>
-                    <div className={"my-3 tag-preview-wrapper"}>
-                        {requestTags.length > 0 ?
-                            <ul className="d-flex justify-content-start flex-wrap row">
-                                {
-                                    requestTags.map((tag, index) =>
-                                        <li
-                                            key={index + 200}
-                                            className="p-2 text-truncate"
-                                        >
-                                            {tag}
-                                        </li>)
-                                }
-                            </ul>
-                            : ''}
-                    </div>
-                    <div className={"imgsPreview-wrapper justify-content-between row "}>
-                        {imgsPreview.map((i, index) => 
-                            <div className="position-relative col-md-6 col-lg-4 my-3 p-0" key={index + 300}>
-                                <img className="" height="96" src={i} />
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <div className="mb-2">
+          <p>
+            Location: <strong>{location}</strong>
+          </p>
+        </div>
+        <div>
+          <p>Medical tests</p>
+          <div className={"my-3 tag-preview-wrapper"}>
+            {requestTags.length > 0 && (
+              <ul className="d-flex justify-content-start flex-wrap row">
+                {requestTags.map((tag, index) => (
+                  <Chip
+                    key={index + 200}
+                    className="m-1"
+                    label={tag}
+                    variant="outlined"
+                  />
+                ))}
+              </ul>
+            )}
+          </div>
+          <div className={"imgsPreview-wrapper justify-content-between row "}>
+            {imgsPreview.map((i, index) => (
+              <div
+                className="position-relative col-md-6 col-lg-4 my-3 p-0"
+                key={index + 300}
+              >
+                <img className="" height="96" src={i} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default RequestCard
+export default RequestCard;
