365b7e5e22c08dee51c38d14bb6af9c53d24473e
Koulinn <drdverzola@gmail.com>
2021-11-13 09:09:24 +0000

    added logic to align message time accordingly to the sender

--- src/components/ChatComponents/ChatMessage.jsx
+++ src/components/ChatComponents/ChatMessage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {format} from 'date-fns'
 
 function ChatMessage({ user, message, img }) {
+    console.log(message.senderRole)
     return (
         <div className={"position-relative w-75 my-3" + (user ? ' align-self-end' : '')}>
             <div className={"px-3 pt-3 pb-2 d-block" + (user ? ' userMessage-wrapper' : ' assistantMessage-wrapper')} >
@@ -14,7 +15,7 @@ function ChatMessage({ user, message, img }) {
                     : ''
                 }
                 <p style={{overflowWrap: 'anywhere'}}>{message.text}</p>
-                <small className="d-block" style={{textAlign:'left'}}>{format(new Date(message.createdAt), 'HH:mm')}</small>
+                <small className="d-block" style={{textAlign:(message.senderRole === 'user' ? 'right' : 'left')}}>{format(new Date(message.createdAt), 'HH:mm')}</small>
             </div>
 
         </div>
